5493db1df4e801bfe16df0279fc100adfe60e99d
Keith March Mistler <kmistler@gmail.com>
2021-07-05 20:30:53 -0400

    addTask

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,7 +1,7 @@
-
+import React, {useState} from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks';
-import React, {useState} from 'react'
+import AddTask from './components/AddTask';
 
 
 const App = () => {
@@ -44,6 +44,7 @@ const App = () => {
   return (
     <div className="container">
       <Header /> 
+      <AddTask />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete= 
         { deleteTask }  onToggle={toggleReminder}/>
